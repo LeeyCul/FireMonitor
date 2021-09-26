bde9e3fe7864a92a6d05d35df11b1ce358ff7804
lihongpei <leeycul@leeyculdeMacBook-Air.local>
2021-09-26 17:15:25 +0800

    fix: 测试

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>这是我自己的测试文件</h1>
+      <h1 className={styles.title}>这是我自己的测试文件1</h1>
     </div>
   );
 }
