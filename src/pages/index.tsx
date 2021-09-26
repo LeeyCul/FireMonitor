a654bee26a430f134025f33060b4398d8422388b
lihongpei <leeycul@leeyculdeMacBook-Air.local>
2021-09-26 15:55:35 +0800

    fix: dd

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,7 @@ import styles from './index.less';
 export default function IndexPage() {
   return (
     <div>
-      <h1 className={styles.title}>Page11 22index11121111222</h1>
+      <h1 className={styles.title}>Page112 222index11121111222</h1>
     </div>
   );
 }
