763d69ea3b73d6b617d380b8251fc844ba47ad96
lihongpei <leeycul@leeyculdeMacBook-Air.local>
2021-10-08 15:08:43 +0800

    feat: 完善结构

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,7 +1,15 @@
 import React from 'react';
+import styles from './style.less';
 
 function Home() {
-  return <div>首页</div>;
+  return (
+    <div className={styles.conainer}>
+      <iframe
+        src="https://maplab.amap.com/share/mapv/d17012314a91523989635d0e50d62fc5"
+        style={{ height: '100%', width: '100%' }}
+      />
+    </div>
+  );
 }
 
 export default Home;
