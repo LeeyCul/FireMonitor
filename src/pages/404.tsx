23858ffd4891dfebeebf43fcfcd8fd6c2f336cfa
lihongpei <leeycul@leeyculdeMacBook-Air.local>
2021-10-29 20:04:24 +0800

    fix: 测试不熟

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Page404() {
-  return <div style={{ background: '#ff00' }}>404页面1</div>;
+  return <div style={{ background: '#ff00' }}>404页面</div>;
 }
 
 export default React.memo(Page404);
