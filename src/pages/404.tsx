83d54729b47a947ded3f64fb8ee0bc4661cae4c8
lihongpei <leeycul@leeyculdeMacBook-Air.local>
2021-10-29 18:43:48 +0800

    fix: 部署

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 function Page404() {
-  return <div style={{ background: '#ff01' }}>404页面</div>;
+  return <div style={{ background: '#ff0000' }}>404页面</div>;
 }
 
 export default React.memo(Page404);
