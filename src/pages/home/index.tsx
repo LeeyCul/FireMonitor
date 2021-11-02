cee62d83895c1119f4a3d06bfeacfce2114ced60
grig <1020342566@qq.com>
2021-11-02 21:47:18 +0800

    chore: 配置no-undef/地图添加四川行政遮罩

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -1,38 +1,54 @@
-import { useCallback, useEffect, useRef, useState } from 'react';
-import { Tabs } from 'antd';
+import { useCallback, useRef, useEffect, useState } from 'react';
 import Amap from '@/common/components/Amap';
 import Page from '@/common/components/Page';
 import DataQuery from './dataQuery';
 import Statistic from './statistic';
 import styles from './style.less';
-import { LevelList } from '@/common/constant';
-// import Iconfont from '@/common/components/IconFont';
+import {} from '@ant-design/pro-layout';
 
-function LevelBar() {
-  return (
-    <div className={styles.levelBar}>
-      <div>等级：</div>
-      {LevelList.map(({ color: backgroundColor, text }, index) => (
-        <div key={index} className={styles.levelBox}>
-          <span className={styles.colorTag} style={{ backgroundColor }} />
-          <span>{text}</span>
-        </div>
-      ))}
-    </div>
-  );
-}
+function Home() {
+  const AmapRef = useRef<any>();
+  const mapRef = useRef<AMap.Map>();
+  const [mapReady, setMapReady] = useState<boolean>(false);
+  const drawDistrict = useCallback((AMap, map) => {
+    const district = new AMap.DistrictSearch({
+      extensions: 'all',
+      level: 'district',
+    });
 
-function ToolBar() {
-  const handleShowData = useCallback(() => {}, []);
-  const handleDownload = useCallback(() => {}, []);
+    AmapRef.current = Amap;
+    mapRef.current = map;
+    setMapReady(true);
+    district.search('四川', (_: any, result: any) => {
+      const bounds = result.districtList[0].boundaries;
+      const polygons = [];
+      if (bounds) {
+        for (let i = 0, l = bounds.length; i < l; i++) {
+          const polygon = new AMap.Polygon({
+            map,
+            strokeWeight: 1,
+            path: bounds[i],
+            fillOpacity: 0.7,
+            fillColor: '#CCF3FF',
+            strokeColor: '#CC66CC',
+          });
+          polygons.push(polygon);
+        }
+        // 地图自适应
+        map.setFitView();
+      }
+    });
+  }, []);
+
+  useEffect(() => {
+    // todo 获取数据
+    console.log(AmapRef.current);
+    // todo 描点
+  }, [mapReady]);
+
   return (
-    <div className={styles.toolBar}>
-      <div className={styles.toolBox} onClick={handleShowData}>
-        <span>数据</span>
-      </div>
-      <div className={styles.toolBox} onClick={handleDownload}>
-        <span>下载</span>
-      </div>
+    <div className={styles.conainer} id="conainer">
+      <Amap mapId="HOMEMAP" onLoadCallback={drawDistrict} />
     </div>
   );
 }
